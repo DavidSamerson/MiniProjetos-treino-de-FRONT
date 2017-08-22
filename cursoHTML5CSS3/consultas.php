1f1059cd3b85d94f27dc2fc4441fba08b9e008c1
DavidSamerson <samerson10@gmail.com>
2017-08-22 16:05:47 -0300

    Testes2

--- cursoHTML5CSS3/consultas.php
+++ cursoHTML5CSS3/consultas.php
@@ -1,45 +1,79 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php
+    include_once("conexao.php");
 
-<head>
-    <meta charset="utf-8">
-    <title>Sistema de Cadastro</title>
-    <link rel="stylesheet" href="css/estilo.css">
-</head>
+        $filtro = isset($_GET['filtro'])?$_GET['filtro']:"";
 
-<body>
-    <div class="container">
-        <nav>
-            <ul class="menu">
-                <a href="index.php">
-                    <li>Cadastro</li>
-                </a>
-                <a href="consultas.php">
-                    <li>
-                     Consultas
-                    </li>
-                </a>
-            </ul>
-        </nav>
-        <section>
-           
-            <h1>Confirmação de Cadastro</h1>
-            <hr>
-            <br><br>
+        $sql = "select * from usuarios where profissao like '%$filtro%' order by nome ";
+        $consulta = mysqli_query($conexao,$sql);
+
+        $registros = mysqli_num_rows($consulta);
+        
+?>
+    <!DOCTYPE html>
+    <html lang="pt-br">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Sistema de Cadastro</title>
+        <link rel="stylesheet" href="css/estilo.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <nav>
+                <ul class="menu">
+                    <a href="index.php">
+                        <li>Cadastro</li>
+                    </a>
+                    <a href="consultas.php">
+                        <li>
+                            Consultas
+                        </li>
+                    </a>
+                </ul>
+            </nav>
+            <section>
+
+                <h1>Consultas</h1>
+                <hr>
+                <br><br>
+
+                <form method="get" action="">
+                    Filtrar por profissão:
+                    <input type="text" name="filtro" class="campo" autofocus required>
+                    <input type="submit" value="pesquisar" class="btn">
+                </form>
+
+                <?php 
+                
+                print "<br><br>";
             
-            <?php 
-            
-                if ($linhas == 1){
-                    print "Cadastro efetuado com sucesso!";
-                } else {
-                    print "Cadastro não efetuado. <br>
-                    Já existe um usuario com este E-MAIL.";
+                print "'$registros' Registros encontrados.";
+                
+                print "<br><br>";
+                
+                while($exibirRegistros = mysqli_fetch_array($consulta)){
+                    
+                    $codigo = $exibirRegistros[0];
+                    $nome = $exibirRegistros[1];
+                    $email =  $exibirRegistros[2];
+                    $profissao = $exibirRegistros[3];
+                    
+                    print "<article>";
+                    
+                    print "$codigo<br>";
+                    print "$nome<br>";
+                    print "$email<br>";
+                    print "$profissao<br>";
+                        
+                    print "</article>";
                 }
-            
+                
+                mysqli_close($conexao);
             ?>
-            
-        </section>
-    </div>
-</body>
 
-</html>
+            </section>
+        </div>
+    </body>
+
+    </html>
